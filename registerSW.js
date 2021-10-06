0df83a34f73bc7a9f7269593d00b061fd08eacae
virzs <2369310227@qq.com>
2021-10-06 13:46:46 +0800

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/Search-Next/sw.js', { scope: '/Search-Next/' })})}
